929dfe0b1405e6ab12e8361f8fb9863d5fe82c6a
Chloe Kelly <chloetay3@gmail.com>
2021-04-27 18:24:38 -0400

    Edit comments

--- calculate.js
+++ calculate.js
@@ -13,6 +13,7 @@ let currentOperation = null;
 let calculationComplete = false;
 let waitingForSecondOperand = false;
 
+
 digitBtns.forEach((button) => {
   button.addEventListener("click", inputDigit);
 });
@@ -79,7 +80,7 @@ function formatNumberWithCommas(number) {
 
 // Changes display value from positive to negative and vice versa
 function changeSign() {
-  let displayValue = getDisplayValue(); // this doesn't work as a global variable because...practice explaining to Jeff
+  let displayValue = getDisplayValue(); 
   if (displayValue === "") {
     setDisplayValue("-");
   } else if (displayValue === "-") {
@@ -95,7 +96,7 @@ function changeSign() {
 
 // Input digits into the display -- updated with decimal validation and digit limit for operands
 function inputDigit(event) {
-  let displayValue = getDisplayValue(); // also cannot be global for same reason as above
+  let displayValue = getDisplayValue(); 
   let clickedButtonValue = event.target.value;
   if (displayValue.includes(".") && clickedButtonValue === "." && waitingForSecondOperand === false) {
     return;
@@ -117,17 +118,33 @@ function inputDigit(event) {
 
 // When operator button is pressed, after inputting the first operand
 // WITHOUT COMMAS
-function handleOperation(event) {
-  let clickedButtonValue = event.target.value;
-  currentOperation = window[clickedButtonValue];
-  operand1 = parseFloat(getDisplayValue());
-  waitingForSecondOperand = true;
-} 
+// function handleOperation(event) {
+//   let clickedButtonValue = event.target.value;
+//   currentOperation = window[clickedButtonValue];
+//   operand1 = parseFloat(getDisplayValue());
+//   waitingForSecondOperand = true;
+// } 
 
 // When operator button is pressed, after inputting the first operand
 // Changed to parseFloat in if statement to deal with decimals
 // WITH COMMAS
 // Doesn't work with negative numbers
+function handleOperation(event) {
+  let clickedButtonValue = event.target.value;
+  currentOperation = window[clickedButtonValue];
+  //console.log(currentOperation);
+  operand1 = getDisplayValue(); 
+  if (operand1.includes(",")) {
+    operand1 = operand1.replace(/,/g, "");
+    operand1 = parseFloat(operand1);
+    console.log(operand1);
+  } else {
+    operand1 = parseFloat(getDisplayValue());
+  }  
+  waitingForSecondOperand = true;
+} 
+
+// Experiment to deal with negative numbers -- regex -- haven't figured out yet
 // function handleOperation(event) {
 //   let clickedButtonValue = event.target.value;
 //   currentOperation = window[clickedButtonValue];
@@ -142,6 +159,7 @@ function handleOperation(event) {
 //   }  
 //   waitingForSecondOperand = true;
 // } 
+
 
 //Functions below are add/subtract/multiply/divide for single pairs of numbers only.
 function add(num1, num2) {
@@ -158,18 +176,18 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   if (num2 === 0) {
-    return (display.textContent = "not possible!"); // this DOES NOT WORK!!! if using setDisplayValue -- won't display the message
+    return display.textContent = "not possible!"; // this DOES NOT WORK!!! if using setDisplayValue -- won't display the message
   } else {
     return num1 / num2;
   }
-}
+} 
 
 //Factorial fx, using for loop
 function factorial(event) {
-  // why is event grayed out?
   let num = parseInt(getDisplayValue());
   if (num > 100) {
-    return setDisplayValue("error"); // if no return, it won't display "error" -- is it skipping to the loop? -- displays "infinity"
+    setDisplayValue("error");
+    return; // if no return, it won't display "error" -- is it skipping to the loop? -- displays "infinity"
   } else if (num === 0 || num === 1) {
     num = 1;
     setDisplayValue(num);
@@ -197,23 +215,23 @@ function operate(operator, num1, num2) {
 
 // Formats the result (if > 1 ) to certain number of sig figs (NOT decimal places) to fit in the display and formats with sci notation. Using toFixed() doesn't limit the decimals nor formats with sci notation.
 // WITHOUT COMMAS
-function formatResult(result) {
-  if (result < 1) {
-    return parseFloat(result.toFixed(7)); // wrapping in parseFloat gets rid of trailing zeros :)
-  } else if (result.toString().length > 9) {
-    return parseFloat(result.toPrecision(9));
-  }
-  return result;
-} 
-
-// Formats the result (if > 1 ) to certain number of sig figs (NOT decimal places) to fit in the display and formats with sci notation. Using toFixed() doesn't limit the decimals nor formats with sci notation.
-// WITH COMMAS
 // function formatResult(result) {
 //   if (result < 1) {
 //     return parseFloat(result.toFixed(7)); // wrapping in parseFloat gets rid of trailing zeros :)
 //   } else if (result.toString().length > 9) {
-//     return formatNumberWithCommas(parseFloat(result.toPrecision(9)));
+//     return parseFloat(result.toPrecision(9));
 //   }
-//   return formatNumberWithCommas(result);
-// }
+//   return result;
+// } 
+
+// Formats the result (if > 1 ) to certain number of sig figs (NOT decimal places) to fit in the display and formats with sci notation. Using toFixed() doesn't limit the decimals nor formats with sci notation.
+// WITH COMMAS
+function formatResult(result) {
+  if (result < 1) {
+    return parseFloat(result.toFixed(7)); // wrapping in parseFloat gets rid of trailing zeros :)
+  } else if (result.toString().length > 9) {
+    return formatNumberWithCommas(parseFloat(result.toPrecision(9)));
+  }
+  return formatNumberWithCommas(result);
+}
 
